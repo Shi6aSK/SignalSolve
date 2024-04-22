788f67ac87f01feee951b6ed73a050c7cabb7125
Shobhit Singh <117563157+Shi6aSK@users.noreply.github.com>
2024-04-22 17:16:19 +0530

    found a working source for bg audio clearing

--- pages/1_CleanAudio.py
+++ pages/1_CleanAudio.py
@@ -1,22 +1,79 @@
 import streamlit as st
-from pydub import AudioSegment
-from io import BytesIO
-import tempfile
 import os
+import logging
+from component.page_meta import page_meta
+from pydub import AudioSegment
+from constants.temporary import TMP_DIR
+from utils.pre_processing import pre_processing
+import noisereduce as nr
+import numpy as np
 
-st.title("Audio Converter")
-uploaded_file = st.file_uploader("Upload an audio file", type=["mp3"])
-audio=st.audio(uploaded_file, format='audio/mp3')
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://raw.githubusercontent.com/Shi6aSK/SignalSolve/main/pages/Try%20Online.png");
+background-size: 100%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
 
-if uploaded_file is not None:
-    st.write("File uploaded successfully!")
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
 
-    # Save uploaded file to a temporary location
-    temp_file = tempfile.NamedTemporaryFile(suffix=".mp3", delete=False)
-    temp_file.write(uploaded_file.read())
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
 
-    # Close the file to release the resources
-    temp_file.close()
-
-    # Convert the temporary file to AudioSegment
-    sound = AudioSegment.from_mp3(temp_file.name)
+UNDER_CONSTRUCTION = False
+page_meta(page_title="Clean Background Noise", page_icon="🎸", is_under_construction=UNDER_CONSTRUCTION)
+if not UNDER_CONSTRUCTION:
+    file = st.file_uploader(label="Upload your audio file", accept_multiple_files=False, type=["mp3", "wav"])
+    if file is not None:
+        btn = st.button("Enhance")
+        if btn:
+            file_name = file.name
+            file_ext = file_name.split(".")[-1]
+            AUDIO_FILE = os.path.join(TMP_DIR, file_name)
+            pre_processing(AUDIO_FILE)
+            with open(AUDIO_FILE, "wb+") as f:
+                f.write(file.read())
+                f.close()
+            file.close()
+            if file_ext == "mp3":
+                src = AUDIO_FILE
+                dst = AUDIO_FILE.replace("mp3", "wav")
+                to_wav = AudioSegment.from_mp3(src)
+                to_wav.export(dst, format="wav")
+                file_name = file_name.replace("mp3", "wav")
+                try:
+                    os.remove(AUDIO_FILE)
+                except OSError:
+                    pass
+                AUDIO_FILE = dst
+            with st.spinner("Loading"):
+                audio = AudioSegment.from_file(AUDIO_FILE)
+                # Convert Pydub AudioSegment object to NumPy array
+                samples = audio.get_array_of_samples()
+                sample_rate = audio.frame_rate
+                reduced_noise = nr.reduce_noise(y=samples, sr=sample_rate)
+                reduced_audio = audio._spawn(reduced_noise.astype(np.int16))
+                ENHANCE_AUDIO = f"enhance_{file_name}"
+                SAVED_AUDIO = os.path.join(TMP_DIR, ENHANCE_AUDIO)
+                reduced_audio.export(SAVED_AUDIO, format="wav")
+            st.success("Completed!")
+            st.markdown("### Before")
+            st.audio(AUDIO_FILE)
+            st.markdown("### After")
+            st.audio(SAVED_AUDIO)
+            logging.info("BACKGROUND_NOISE_REDUCTION")
+            print("BACKGROUND_NOISE_REDUCTION")
+            try:
+                os.remove(SAVED_AUDIO)
+                os.remove(AUDIO_FILE)
+            except OSError:
+                print("Can't remove")
+                pass
